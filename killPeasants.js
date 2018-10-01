ccfdf04b80a5d3efc196b05669b7f7d2f399c3c9
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-09-30 21:41:00 -0400

    Update killPeasants.js

--- killPeasants.js
+++ killPeasants.js
@@ -1,14 +1,12 @@
+
 function kill(){
-var container = document.getElementsByClassName("content-main AdaptiveSearchTimeline");
+var container = document.getElementsByClassName("stream-container");
 var itm = container[0].getElementsByClassName("js-stream-item stream-item stream-item");
 for(var i=0; i<itm.length; i++) {
 	var h = itm[i].getElementsByClassName("stream-item-header");
-	var promo = h[0].getElementsByClassName("Icon Icon--promoted");
+	var promo = itm[i].getElementsByClassName("Icon Icon--promoted");
 	var verif = h[0].getElementsByClassName("Icon Icon--verified");
-	if(verif.length == 0) {
-		if(promo.length < 1 && verif.length > 0) {
-			itm[i].parentElement.removeChild(itm[i]);
-		}
+	if(promo.length == 1  || verif.length == 0) {
 		itm[i].parentElement.removeChild(itm[i]);
     }
 }
